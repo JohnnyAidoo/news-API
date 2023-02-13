87acb1adc265ea8a872b3711444b6c42999db59e
Johnny Osei Aidoo <johnnyoseiaidoo65@gmail.com>
2023-02-13 15:11:40 +0000

    'commit

---
+++ fetch.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+JsonObject = {}
+
+link = 'https://news.google.com/home?hl=en-GH&gl=GH&ceid=GH:en'
+req = requests.get(link)
+
+soup = BeautifulSoup(req.content, 'html.parser')
+
+finder = soup.find_all("a", class_ ='WwrzSb')
+img_finder = soup.find_all('img', class_='Quavad')
+
+
+for text,img in zip(finder,img_finder):
+    head = (text['aria-label'])
+#    print(head)
+    url_link = (text['href'])
+#    print(url_link)
+    url_image = (img['srcset'])
+#    print(url_image)
+    JsonObject.append({
+        "header": head,
+        "url": url_link,
+        "image": url_image
+    })
+
+data = json.dumps(JsonObject)
+print(data)
+print('done')
