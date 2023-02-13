be4c1fc53aa282d2b62ab263a37a345f2f7d1506
Johnny Osei Aidoo <johnnyoseiaidoo65@gmail.com>
2023-02-13 14:58:23 +0000

    'helo'

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask
+import json
 
-json = {}
+jsonObject = {
+        "head":"",
+        "url" :"",
+        "image": ""
+        }
 
 link = 'https://news.google.com/home?hl=en-GH&gl=GH&ceid=GH:en'
 req = requests.get(link)
 
 soup = BeautifulSoup(req.content, 'html.parser')
 
-finder = soup.find("a", class_ ='WwrzSb')
-img_finder = soup.find('img', class_='Quavad')
+finder = soup.find_all("a", class_ ='WwrzSb')
+img_finder = soup.find_all('img', class_='Quavad')
 
 
 for text,img in zip(finder,img_finder):
-    head = (text['arial-label'])
-    print(head)
+    head = (text['aria-label'])
+#    print(head)
     url_link = (text['href'])
-    print(url)
-    print(img['srcset'])
+#    print(url_link)
+    url_image = (img['srcset'])
+#    print(url_image)
+
 print('done')
+
+json.dump(jsonObject)
